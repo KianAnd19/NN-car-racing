1cd89d7dd2b9d46d2a81eeb09a66fe4e793a1772
Kian <24872849@sun.ac.za>
2024-06-23 15:49:14 +0200

    Running through cart pole example from openai gym

---
+++ run.py
@@ -0,0 +1,40 @@
+import gymnasium as gym
+import torch
+import torch.nn.functional as F
+
+loaded_policy_net = torch.load('model.pth')
+
+def select_action_run(state):
+    with torch.no_grad():
+        return loaded_policy_net(state).max(1).indices.view(1, 1)
+
+device = torch.device(
+    "cuda" if torch.cuda.is_available() else
+    "mps" if torch.backends.mps.is_available() else
+    "cpu"
+)
+
+# Create a new environment for visualization
+env = gym.make("CartPole-v1", render_mode="human")
+state, info = env.reset()
+state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
+done = False
+
+total_reward = 0
+steps = 0
+
+for i in range(10000):
+    action = select_action_run(state)
+    observation, reward, terminated, truncated, _ = env.step(action.item())
+    reward = torch.tensor([reward], device=device)
+    done = terminated or truncated
+    
+    next_state = torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0)
+    state = next_state
+    
+    total_reward += reward.item()
+    steps += 1
+
+env.close()
+
+print(f"Model performance: Total steps: {steps}, Total reward: {total_reward}")
